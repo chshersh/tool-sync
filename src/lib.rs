46828ea8d4f45c7ae674b61b5126e36760b22fd7
Dmitrii Kovanikov <kovanikov@gmail.com>
2022-08-16 15:06:35 +0300

    Resolve config path variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,38 @@
 mod cli;
 
+use std::path::PathBuf;
+use std::process;
 use clap::Parser;
+use dirs;
 
 use cli::Cli;
 
+const DEFAULT_CONFIG_PATH: &str = ".tool.toml";
+
 pub fn run() {
-    let args = Cli::parse();
-    println!("{:?}", args);
+    let cli = Cli::parse();
+
+    println!("{:?}", cli);
+
+    let config_path = resolve_config_path(cli.config);
+
+    println!("Config path: {}", config_path.display());
+}
+
+fn resolve_config_path(config_path: Option<PathBuf>) -> PathBuf {
+    match config_path {
+        Some(path) => path,
+        None => match dirs::home_dir() {
+            Some(home_path) => {
+                let mut path = PathBuf::new();
+                path.push(home_path);
+                path.push(DEFAULT_CONFIG_PATH);
+                path
+            }
+            None => {
+                eprintln!("Unable to find $HOME directory!");
+                process::exit(1);
+            }
+        }
+    }
 }
