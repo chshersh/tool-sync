3bb4240f0fefb0e11282e64361c4631d11a46382
Mitchell Berendhuysen <mitchellbhuysen@hotmail.com>
2022-09-15 19:10:56 +0200

    incorporated feedback and reordered main cli match

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,16 @@ mod model;
 mod sync;
 
 use std::collections::BTreeMap;
+use std::fmt::Write;
 use std::path::PathBuf;
 
 use clap::Parser;
 
 use crate::config::cli::{Cli, Command};
-use crate::config::schema::ConfigAsset;
+use crate::config::schema::{Config, ConfigAsset};
 use crate::config::template;
 use crate::config::toml;
-use crate::sync::db::lookup_tool;
+use crate::sync::db::{build_db, lookup_tool};
 use crate::sync::sync;
 
 const DEFAULT_CONFIG_PATH: &str = ".tool.toml";
@@ -23,6 +24,7 @@ pub fn run() {
     let config_path = resolve_config_path(cli.config.clone());
 
     match cli.command {
+        Command::DefaultConfig => generate_config(),
         Command::Sync => match toml::parse_file(&config_path) {
             Err(e) => {
                 err::abort_with(&format!(
@@ -31,20 +33,43 @@ pub fn run() {
                     e.display()
                 ));
             }
-            Ok(tool) => {
-                sync(tool);
+            Ok(config) => {
+                sync(config);
             }
         },
-        Command::DefaultConfig => generate_config(),
         Command::Install { name } => {
-            if let Some(tool_info) = lookup_tool(&name) {
-                if let Ok(mut tool) = toml::parse_file(&config_path) {
+            with_parsed_file(&config_path, |mut config| {
+                if let Some(tool_info) = lookup_tool(&name) {
                     let tool_btree: BTreeMap<String, ConfigAsset> =
                         BTreeMap::from([(name, tool_info.into())]);
-                    tool.tools = tool_btree;
-                    sync(tool);
-                }
-            }
+                    config.tools = tool_btree;
+                    sync(config);
+                } else {
+                    let mut exit_message: String =
+                        format!("Unknown tool: {}\nSupported tools:\n", name);
+                    for tool in build_db().keys().cloned().collect::<Vec<String>>() {
+                        if let Err(e) = writeln!(exit_message, "\t* {}", tool) {
+                            err::abort_suggest_issue(&format!("{}", e));
+                        };
+                    }
+                    err::abort_with(&exit_message);
+                };
+            });
+        }
+    }
+}
+
+fn with_parsed_file<F: FnOnce(Config)>(config_path: &PathBuf, on_success: F) {
+    match toml::parse_file(config_path) {
+        Ok(config) => {
+            on_success(config);
+        }
+        Err(e) => {
+            err::abort_with(&format!(
+                "Error parsing configuration at path {}: {}",
+                config_path.display(),
+                e.display()
+            ));
         }
     }
 }
