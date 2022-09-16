70366cb31c5be00c85c0dd9e586679537d989ed0
Dmitrii Kovanikov <kovanikov@gmail.com>
2022-09-16 13:09:41 +0100

    Comment in the beginning of lines

--- src/config/template.rs
+++ src/config/template.rs
@@ -25,22 +25,22 @@ fn config_template() -> String {
 # You can configure the installation of any tool by specifying corresponding options:
 #
 # [ripgrep]  # Name of the tool (new or one of the hardcoded to override default settings)
-    # owner     = "BurntSushi"  # GitHub repository owner
-    # repo      = "ripgrep"     # GitHub repository name
-    # exe_name  = "rg"          # Executable name inside the asset
+#     owner     = "BurntSushi"  # GitHub repository owner
+#     repo      = "ripgrep"     # GitHub repository name
+#     exe_name  = "rg"          # Executable name inside the asset
 
-    # Uncomment to download a specific version or tag.
-    # Without this tag latest will be used
-    # tag       = "13.0.0"
+#     Uncomment to download a specific version or tag.
+#     Without this tag latest will be used
+#     tag       = "13.0.0"
 
-    # Asset name to download on linux OSes
-    # asset_name.linux = "x86_64-unknown-linux-musl"
+#     Asset name to download on linux OSes
+#     asset_name.linux = "x86_64-unknown-linux-musl"
 
-    # Uncomment if you want to install on macOS as well
-    # asset_name.macos = "apple-darwin"
+#     Uncomment if you want to install on macOS as well
+#     asset_name.macos = "apple-darwin"
 
-    # Uncomment if you want to install on Windows as well
-    # asset_name.windows = "x86_64-pc-windows-msvc""###,
+#     Uncomment if you want to install on Windows as well
+#     asset_name.windows = "x86_64-pc-windows-msvc""###,
         version = env!("CARGO_PKG_VERSION"),
     )
 }
