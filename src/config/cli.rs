fafb893b185f05b4005fabb36a7460636e85f7e9
Mitchell Berendhuysen <mitchellbhuysen@hotmail.com>
2022-10-14 15:24:21 +0200

    added help text

--- src/config/cli.rs
+++ src/config/cli.rs
@@ -18,7 +18,49 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
-    /// Help text
+    /// Generate shell completion scripts for GitHub CLI commands.
+    ///
+    /// You will need to set up completions manually, follow the instructions below. The exact
+    /// config file locations might vary based on your system. Make sure to restart your
+    /// shell before testing whether completions are working.
+    ///
+    /// ### bash
+    ///
+    /// First, ensure that you install `bash-completion` using your package manager.
+    ///
+    /// After, add this to your `~/.bash_profile`:
+    ///
+    ///     eval "$(tool completion bash)"
+    ///
+    /// ### zsh
+    ///
+    /// Generate a `_tool` completion script and put it somewhere in your `$fpath`:
+    ///
+    ///     tool completion zsh > /usr/local/share/zsh/site-functions/_tool
+    ///
+    /// Ensure that the following is present in your `~/.zshrc`:
+    ///
+    ///     autoload -U compinit
+    ///     compinit -i
+    ///
+    /// Zsh version 5.7 or later is recommended.
+    ///
+    /// ### fish
+    ///
+    /// Generate a `tool.fish` completion script:
+    ///
+    ///     tool completion fish > ~/.config/fish/completions/tool.fish
+    ///
+    /// ### PowerShell
+    ///
+    /// Open your profile script with:
+    ///
+    ///     mkdir -Path (Split-Path -Parent $profile) -ErrorAction SilentlyContinue
+    ///     notepad $profile
+    ///
+    /// Add the line and save the file:
+    ///
+    ///     Invoke-Expression -Command $(tool completion powershell | Out-String)
     Completion { shell: clap_complete::Shell },
 
     /// Sync all tools specified in configuration file or the only one specified in the command line
